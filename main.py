ac2766bb52ecbeda29e47116801683a4c4873254
Saleh Mehdikhani <saleh.mehdikhani@unikie.com>
2023-09-24 23:15:08 +0300

    Read praying times from Excel files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import string
 
 import requests
 import schedule
@@ -9,12 +10,14 @@ import sys
 import os
 from enum import Enum
 from datetime import date
+import openpyxl
 
 
 class Salat(Enum):
     Fajr = 1
     Dhuhr = 2
     Maghrib = 3
+
 
 '''
 Read the configuration file
@@ -47,9 +50,11 @@ except Exception as e:
 # Replace These variables with the proper one fits to your needs
 TELEGRAM_BOT_TOKEN = data["telegram"]["bot_token"]
 TELEGRAM_CHAT_ID = data["telegram"]["chat_id"]
-CITY = data["aladhan"]["city"]
-COUNTRY = data["aladhan"]["country"]
-ADHAN_METHOD = data["aladhan"]["method"]
+SOURCE_MODE = data["source"]["mode"]
+EXCEL_FILES_PATH = data["source"]["excel"]["root_path"]
+CITY = data["source"]["aladhan"]["city"]
+COUNTRY = data["source"]["aladhan"]["country"]
+ADHAN_METHOD = data["source"]["aladhan"]["method"]
 '''
 Methods:
 Muslim World League (MWL) - Method value: "1"
@@ -151,9 +156,19 @@ def schedule_next_praying_time():
 
 
 def get_prayer_times(date_time: datetime):
+    if SOURCE_MODE == "excel":
+        return get_prayer_times_excel(date_time)
+    elif SOURCE_MODE == "aladhan":
+        return get_prayer_times_aladhan(date_time)
+    else:
+        print(f"The source mode ({SOURCE_MODE}) is invalid")
+        return None
+
+
+def get_prayer_times_aladhan(date_time: datetime):
     # Make a request to the Aladhan API
     data_str = date_time.strftime('%d-%m-%Y')
-    url = f"http://api.aladhan.com/v1/timingsByCity/{data_str}?"\
+    url = f"http://api.aladhan.com/v1/timingsByCity/{data_str}?" \
           f"city={CITY}&country={COUNTRY}&method={ADHAN_METHOD}"
     print(url)
     response = requests.get(url)
@@ -169,7 +184,7 @@ def get_prayer_times(date_time: datetime):
         # Define the format of the date and time string
         date_time_format = "%Y-%m-%d %H:%M"
         # Get the current date as a string (you can adjust the date as needed)
-        current_date = time.strftime("%Y-%m-%d")
+        current_date = date_time.strftime("%Y-%m-%d")
         for prayer_name, prayer_time in prayer_times_str.items():
             # Combine the date and time string
             date_time_string = f"{current_date} {prayer_time}"
@@ -182,6 +197,56 @@ def get_prayer_times(date_time: datetime):
     else:
         print(f'Error fetching prayer times: ({response.status_code}).')
         return None
+
+
+def get_prayer_times_excel(date_time: datetime):
+    DATE_OFFSET = 1
+    LAST_FROZEN_MONTH = 4
+    LAST_FROZEN_DAY = 26
+    month = date_time.month
+    day = date_time.day
+
+    # get the file name based on the parameters
+    file_name = f"{EXCEL_FILES_PATH}/{month}.xlsx"
+
+    print("file name: ", file_name)
+
+    # load the excel sheet
+    dataframe = openpyxl.load_workbook(file_name)
+    sheet_obj = dataframe.active
+
+    # set row number based on the file format and the parmeters
+    row = day + DATE_OFFSET
+
+    salat_names = ["Fajr", "Sunrise", "Dhuhr", "Sunset", "Maghrib", "Midnight"]
+
+    # convert from string to time_struct format
+    prayer_times = {}
+    # Define the format of the date and time string
+    date_time_format = "%Y-%m-%d %H:%M:%S"
+    # Get the current date as a string (you can adjust the date as needed)
+    current_date = date_time.strftime("%Y-%m-%d")
+    for item in range(len(salat_names)):
+        col = item + 2
+        prayer_time_cell = sheet_obj.cell(row=row, column=col)
+        prayer_time = prayer_time_cell.value
+        if prayer_time == '*':
+            last_valid = \
+                get_prayer_times_excel(datetime.datetime(date_time.year,
+                                                         LAST_FROZEN_MONTH,
+                                                         LAST_FROZEN_DAY,
+                                                         00, 00))
+            prayer_time = \
+                f"{last_valid[salat_names[item]].tm_hour}:" \
+                f"{last_valid[salat_names[item]].tm_min}:00"
+        # Combine the date and time string
+        date_time_string = f"{current_date} {prayer_time}"
+        prayer_times[salat_names[item]] = datetime.datetime.strptime(
+            date_time_string,
+            date_time_format).timetuple()
+
+    # Print prayer times
+    return prayer_times
 
 
 # Keep the script running to continue scheduling messages
